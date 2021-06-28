8395d6c80a2cb48bcfc3e9bfb43d84893dd38d18
snehomoy100 <snehomoy100@gmail.com>
2021-06-28 18:48:25 +0530

    calculator named react app created & key pressing feature added

---
+++ calculator-app/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+const App = () => {
+
+    const [result, setResult] = useState("hii");
+
+    return (
+        <>
+            <div className="container">
+                <form>
+                    <input type="text" value={result}></input>
+                </form>
+
+                <div className="keypad">
+
+                    <button>Clear</button>
+                    <button>C</button>
+                    <button>&divide;</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>&time;</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>&ndash;</button>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>+</button>
+                    <button>0</button>
+                    <button>.</button>
+                    <button>=</button>
+
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default App;
